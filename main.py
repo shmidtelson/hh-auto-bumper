f5ec858b0e1f62ad51adba0717b029fa11453d2d
Romuald Shmidtelson <d-w-b@mail.ru>
2021-04-09 13:30:36 +0300

    added message `server was started`

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import time
 
 from dotenv import load_dotenv, find_dotenv
 from classes.refresher import Refresher
+from classes.utils.logger import logger
 
 load_dotenv(find_dotenv())
 
 if __name__ == "__main__":
     while True:
-        print("Will start server")
+        logger.info('Server was started')
         client = Refresher()
         client.execute()
         time.sleep(60*5)
